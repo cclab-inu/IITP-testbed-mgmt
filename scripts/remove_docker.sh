d547cfcc085eab3c43ec1d4d3bfbb2446ef22129
Minjun Seong <minjunseong@naver.com>
2022-11-05 04:33:23 +0900

    Remove script commit

--- scripts/remove_docker.sh
+++ scripts/remove_docker.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+# Docker image stop & remove
 docker stop $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
+docker rm -f $(docker ps -a -q)
+docker rmi -f $(docker images -a -q)
+docker network prune -f
 
 systemctl stop docker.service
 systemctl stop containerd.service
 
-sudo apt list --installed | grep docker
+# Docker uninstallation
+# Check the docker is installed
+dpkg -l | grep -i docker
 
-#editing
+# Docker uninstall
+sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli
+sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce
+
+# Remove the files that related docker
+sudo rm -rf /var/lib/docker /etc/docker
+sudo rm /etc/apparmor.d/docker
+sudo groupdel docker
+sudo rm -rf /var/run/docker.sock
+
+# All the Docker misc files remove
+# Caution about general files that named 'docker'
+# sudo find / -name "*docker*" -exec `rm -rf` {} +
