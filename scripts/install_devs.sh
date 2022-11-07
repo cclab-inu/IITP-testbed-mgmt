4eb8c99f0a339ce1036e91f37ed50d55842be48d
milin3t <minjunseong@naver.com>
2022-11-07 21:48:03 +0900

    Updated install_devs.sh

--- scripts/install_devs.sh
+++ scripts/install_devs.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+# Ubuntu Upgrade
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install wget -y
+
 # net-tools install
-sudo apt install net-tools
+sudo apt-get install net-tools -y
+
+# vim install
+sudo apt-get install vim -y
+
+# openSSH install
+sudo apt-get install openssh-server -y
 
 # temporarily using build files directory
 mkdir -p /tmp/tools
 cd /tmp/tools
-
-# Ubuntu Upgrade
-sudo apt update && sudo apt upgrade -y
-sudo apt-get install wget -y
 
 # Installation Go Binaries
 goBinary=$(curl -s https://go.dev/dl/ | grep linux | head -n 1 | cut -d'"' -f4 | cut -d"/" -f3)
