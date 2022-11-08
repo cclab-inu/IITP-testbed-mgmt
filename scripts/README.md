9095291a6be992257219ea8a9dc429da303d7cd9
Minjun Seong <minjunseong@naver.com>
2022-11-08 16:57:24 +0900

    install_devs script debugged

---
+++ scripts/README.md
@@ -0,0 +1,8 @@
+# Manual for using scripts.
+
+* Excecution order
+  - install_devs.sh
+  - install_docker.sh
+  - install_k8s.sh
+  - create_cluster.sh
+  - install_helm.sh
--- scripts/install_devs.sh
+++ scripts/install_devs.sh
@@ -17,15 +17,20 @@ sudo apt-get install openssh-server -y
 mkdir -p /tmp/tools
 cd /tmp/tools
 
-# Installation Go Binaries
-goBinary=$(curl -s https://go.dev/dl/ | grep linux | head -n 1 | cut -d'"' -f4 | cut -d"/" -f3)
-wget --quiet https://dl.google.com/go/$goBinary -O /tmp/build/$goBinary
-sudo tar -C /usr/local -xzf /tmp/build/$goBinary
+# Installation Go Binaries (if you want recent version of golang, uncomment under lines and comment fixed version download lines)
+# goBinary=$(curl -s https://go.dev/dl/ | grep linux | head -n 1 | cut -d'"' -f4 | cut -d"/" -f3)
+# wget --quiet https://dl.google.com/go/$goBinary -O /tmp/tools/$goBinary
+# sudo tar -C /usr/local -xzf /tmp/tools/$goBinary
+
+# Installation Go Binaries (Fixed Verision : 1.18.8)
+wget --quiet https://go.dev/dl/go1.18.8.linux-amd64.tar.gz -O /tmp/tools/go1.18.8.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf /tmp/tools/go1.18.8.linux-amd64.tar.gz
 
 # Environment var settings
 echo "export GOPATH=\$HOME/go" >> ~/.profile
 echo "export GOROOT=/usr/local/go" >> ~/.profile
 echo "export PATH=\$PATH:/usr/local/go/bin:\$HOME/go/bin" >> ~/.profile
+. ~/.profile
 
 # Installation Apparmor
 sudo apt-get install -y apparmor apparmor-utils
